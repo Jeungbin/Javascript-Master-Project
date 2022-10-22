f7c34cb21657f7c61f30bd8a29aa5f596d9aa261
jeongbin <qls2467@gmail.com>
2022-10-22 08:13:30 -0600

    Init

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,13 +1,1 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById('btn')
-const color = document.querySelector('.color')
-
-btn.addEventListener('click', function(){
-    const randomNumber = getrandom();
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-})
-
-function getrandom(){
-    return Math.floor(Math.random()* colors.length);
-}
