65a1d92c84588d32824c977cac2a180bf19135ab
jeongbin <qls2467@gmail.com>
2022-10-24 12:55:13 -0600

    Completed

--- 01-color-flipper/app.js
+++ 01-color-flipper/app.js
@@ -1,1 +1,11 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const changeBtn = document.getElementById("btn");
+const colorName = document.getElementById("color-name");
+
+function changeColor() {
+  let randomNum = Math.floor(Math.random() * 4);
+  colorName.innerHTML = colors[randomNum];
+  document.body.style.background = colors[randomNum];
+}
+changeBtn.addEventListener("click", changeColor);
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,17 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const colorName = document.getElementById("color");
+
+const changeColor = () => {
+  newName = "";
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * 16);
+    newName = newName + hex[randomNum];
+  }
+  console.log(newName);
+  colorName.innerHTML = "#" + newName;
+  document.body.style.background = "#" + newName;
+};
+
+btn.addEventListener("click", changeColor);
